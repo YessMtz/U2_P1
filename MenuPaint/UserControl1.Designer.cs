396ff829a1f6c3fcb106d31d59fc76bceaedb589
Yessenia Martinez <l21211990@tectijuana.edu.mx>
2023-05-09 02:47:53 -0700

    Yess

--- MenuPaint/UserControl1.Designer.cs
+++ MenuPaint/UserControl1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace MenuPaint
 {
-    partial class UserControl1
+    partial class BarraHerramientas
     {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -28,11 +28,176 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BarraHerramientas));
+            this.Colores = new System.Windows.Forms.Button();
+            this.Cubeta = new System.Windows.Forms.Button();
+            this.Lapiz = new System.Windows.Forms.Button();
+            this.Borrador = new System.Windows.Forms.Button();
+            this.Linea = new System.Windows.Forms.Button();
+            this.Rect = new System.Windows.Forms.Button();
+            this.Circulo = new System.Windows.Forms.Button();
+            this.Guardar = new System.Windows.Forms.Button();
+            this.Nuevo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Colores
+            // 
+            this.Colores.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.Colores.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Colores.BackgroundImage")));
+            this.Colores.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Colores.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Colores.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Colores.Location = new System.Drawing.Point(20, 22);
+            this.Colores.Name = "Colores";
+            this.Colores.Size = new System.Drawing.Size(100, 100);
+            this.Colores.TabIndex = 0;
+            this.Colores.UseVisualStyleBackColor = false;
+            // 
+            // Cubeta
+            // 
+            this.Cubeta.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.Cubeta.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Cubeta.BackgroundImage")));
+            this.Cubeta.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Cubeta.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Cubeta.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Cubeta.Location = new System.Drawing.Point(136, 22);
+            this.Cubeta.Name = "Cubeta";
+            this.Cubeta.Size = new System.Drawing.Size(100, 100);
+            this.Cubeta.TabIndex = 1;
+            this.Cubeta.UseVisualStyleBackColor = false;
+            // 
+            // Lapiz
+            // 
+            this.Lapiz.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.Lapiz.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Lapiz.BackgroundImage")));
+            this.Lapiz.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Lapiz.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Lapiz.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Lapiz.Location = new System.Drawing.Point(255, 22);
+            this.Lapiz.Name = "Lapiz";
+            this.Lapiz.Size = new System.Drawing.Size(100, 100);
+            this.Lapiz.TabIndex = 2;
+            this.Lapiz.UseVisualStyleBackColor = false;
+            this.Lapiz.Click += new System.EventHandler(this.Lapiz_Click);
+            this.Lapiz.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Lapiz_ClickDown);
+            this.Lapiz.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Lapiz_ClickMove);
+            this.Lapiz.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Lapiz_ClickUp);
+            // 
+            // Borrador
+            // 
+            this.Borrador.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.Borrador.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Borrador.BackgroundImage")));
+            this.Borrador.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Borrador.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Borrador.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Borrador.Location = new System.Drawing.Point(375, 22);
+            this.Borrador.Name = "Borrador";
+            this.Borrador.Size = new System.Drawing.Size(100, 100);
+            this.Borrador.TabIndex = 3;
+            this.Borrador.UseVisualStyleBackColor = false;
+            this.Borrador.Click += new System.EventHandler(this.Borrador_Click);
+            // 
+            // Linea
+            // 
+            this.Linea.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.Linea.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Linea.BackgroundImage")));
+            this.Linea.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Linea.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Linea.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Linea.Location = new System.Drawing.Point(497, 22);
+            this.Linea.Name = "Linea";
+            this.Linea.Size = new System.Drawing.Size(100, 100);
+            this.Linea.TabIndex = 4;
+            this.Linea.UseVisualStyleBackColor = false;
+            this.Linea.Click += new System.EventHandler(this.Linea_Click);
+            // 
+            // Rect
+            // 
+            this.Rect.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.Rect.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Rect.BackgroundImage")));
+            this.Rect.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Rect.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Rect.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Rect.Location = new System.Drawing.Point(620, 22);
+            this.Rect.Name = "Rect";
+            this.Rect.Size = new System.Drawing.Size(100, 100);
+            this.Rect.TabIndex = 5;
+            this.Rect.UseVisualStyleBackColor = false;
+            this.Rect.Click += new System.EventHandler(this.Rect_Click);
+            // 
+            // Circulo
+            // 
+            this.Circulo.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.Circulo.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Circulo.BackgroundImage")));
+            this.Circulo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Circulo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Circulo.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Circulo.Location = new System.Drawing.Point(745, 22);
+            this.Circulo.Name = "Circulo";
+            this.Circulo.Size = new System.Drawing.Size(100, 100);
+            this.Circulo.TabIndex = 6;
+            this.Circulo.UseVisualStyleBackColor = false;
+            this.Circulo.Click += new System.EventHandler(this.Circulo_Click);
+            // 
+            // Guardar
+            // 
+            this.Guardar.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.Guardar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Guardar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Guardar.Font = new System.Drawing.Font("Bonita", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Guardar.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Guardar.Location = new System.Drawing.Point(923, 22);
+            this.Guardar.Name = "Guardar";
+            this.Guardar.Size = new System.Drawing.Size(100, 49);
+            this.Guardar.TabIndex = 7;
+            this.Guardar.Text = "GUARDAR";
+            this.Guardar.UseVisualStyleBackColor = false;
+            this.Guardar.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // Nuevo
+            // 
+            this.Nuevo.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.Nuevo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Nuevo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Nuevo.Font = new System.Drawing.Font("Bonita", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Nuevo.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Nuevo.Location = new System.Drawing.Point(923, 77);
+            this.Nuevo.Name = "Nuevo";
+            this.Nuevo.Size = new System.Drawing.Size(100, 49);
+            this.Nuevo.TabIndex = 8;
+            this.Nuevo.Text = "NUEVO";
+            this.Nuevo.UseVisualStyleBackColor = false;
+            // 
+            // BarraHerramientas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.Controls.Add(this.Nuevo);
+            this.Controls.Add(this.Guardar);
+            this.Controls.Add(this.Circulo);
+            this.Controls.Add(this.Rect);
+            this.Controls.Add(this.Linea);
+            this.Controls.Add(this.Borrador);
+            this.Controls.Add(this.Lapiz);
+            this.Controls.Add(this.Cubeta);
+            this.Controls.Add(this.Colores);
+            this.Name = "BarraHerramientas";
+            this.Size = new System.Drawing.Size(1055, 150);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button Colores;
+        private System.Windows.Forms.Button Cubeta;
+        private System.Windows.Forms.Button Lapiz;
+        private System.Windows.Forms.Button Borrador;
+        private System.Windows.Forms.Button Linea;
+        private System.Windows.Forms.Button Rect;
+        private System.Windows.Forms.Button Circulo;
+        private System.Windows.Forms.Button Guardar;
+        private System.Windows.Forms.Button Nuevo;
     }
 }
--- MenuPaint/UserControl1.cs
+++ MenuPaint/UserControl1.cs
@@ -10,11 +10,157 @@ using System.Windows.Forms;
 
 namespace MenuPaint
 {
-    public partial class UserControl1: UserControl
+    public partial class BarraHerramientas: UserControl
     {
-        public UserControl1()
+
+
+        //VARIABLES PARA DIBUJAR
+        Bitmap bmLapiz;
+        Graphics gLapiz;
+        bool paint = false;
+        Point px, py;
+
+        //objeto lapiz de color negro y grosor 1
+        Pen lapiz = new Pen(Color.Black, 1);
+
+        //objeto lapiz borrador y grosor 10
+        Pen borrador = new Pen(Color.White, 10);
+
+        //VARIABLES PARA LOS PUNTOS DEL CIRCULO
+        int x, y, sx, sy, cx, cy;
+
+        int index;
+        public BarraHerramientas()
         {
             InitializeComponent();
+            bmLapiz = new Bitmap(Lapiz.Width, Lapiz.Height);
+            gLapiz = Graphics.FromImage(bmLapiz);
+            
+            //CAMBIA EL COLOR DEL LAPIZ PARA BORRAR
+            gLapiz.Clear(Color.White);
+
+            Lapiz.Image = bmLapiz;
         }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+
+        }
+        // ACCIONES DEL BOTON
+        
+        private void Lapiz_ClickUp(object sender, MouseEventArgs e)
+        {
+            paint = false;
+
+            sx = x - cx;
+            sy = y - cy;
+            
+            //PARA DIBUJAR EL CIRCULO
+            if(index ==3)
+            {
+                gLapiz.DrawEllipse(lapiz,cx, cy, sx, sy);
+            }
+
+            //PARA DIBUJAR EL RECTANGULO
+            if(index ==4)
+            {
+                gLapiz.DrawRectangle(lapiz, cx, cy, sx, sy);
+            }
+            
+            //PARA DIBUJAR LINEA RECTA
+            if(index==5)
+            {
+                gLapiz.DrawLine(lapiz, cx, cy, x, y);
+            }
+
+        }
+
+        
+        //TAMANO DE LA LINEA
+        private void Lapiz_Click(object sender, EventArgs e)
+        {
+            index = 1;
+        }
+
+        
+        private void Lapiz_Click(object sender, MouseEventArgs e)
+        {
+
+        }
+        //DIBUJAR
+        private void Lapiz_ClickDown(object sender, MouseEventArgs e)
+        {
+            //si pinta hacia abajo
+            paint = true;
+            py = e.Location;
+
+            //DIBUJAR EL CIRCULO EN LAS DIMENSIONES X & Y
+            cx = e.X;
+            cy = e.Y;
+        }
+
+        private void Lapiz_ClickMove(object sender, MouseEventArgs e)
+        {
+            if(paint)
+            {
+                //para dibujar a mano alzada
+                if(index ==1)
+                {
+                    //punto x en localizacion
+                    px = e.Location;
+                    //pintar una linea tipo Glapiz
+                    gLapiz.DrawLine(lapiz, px, py);
+                    py = px;
+
+                }
+
+                //para borrar lo que se dibujo
+                if (index == 2)
+                {
+                    //punto x en localizacion
+                    px = e.Location;
+                    //pintar una linea tipo PEN+BORRADOR
+                    gLapiz.DrawLine(borrador, px, py);
+                    py = px;
+
+                }
+                Lapiz.Refresh();
+
+                //variables para el ciculo 
+                x = e.X;
+                y = e.Y;
+                sx = e.X - cx;
+                sy = e.Y - cy;
+            }
+        }
+        
+        //ACCIONES DEL BORRADOR
+        private void Borrador_Click(object sender, EventArgs e)
+        {
+            //usamos el index para bottar que es e tamano de 
+            index = 2;
+        }
+
+        //DIBUJAR CIRCULO
+        private void Circulo_Click(object sender, EventArgs e)
+        {
+            index = 3;
+        }
+
+        //DIBUJAR RECTANGULO
+        private void Rect_Click(object sender, EventArgs e)
+        {
+            index = 4;
+        }
+        
+        //DIBUJAR LINEA RECTA
+        private void Linea_Click(object sender, EventArgs e)
+        {
+            index = 5;
+        }
+
+
+
+       
     }
 }
